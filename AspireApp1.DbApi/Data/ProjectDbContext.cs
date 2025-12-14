e278e0294481ccc9a18ed34c79ddf3328d089166
Eiji Tomizawa <tomizawa@logit.co.jp>
2025-12-14 14:01:59 +0900

    bugfix

--- AspireApp1.DbApi/Data/ProjectDbContext.cs
+++ AspireApp1.DbApi/Data/ProjectDbContext.cs
@@ -8,5 +8,18 @@ namespace AspireApp1.DbApi.Data
         public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options) { }
 
         public DbSet<Project> Projects { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Use lowercase table name to match PostgreSQL default identifier folding.
+            modelBuilder.Entity<Project>(b =>
+            {
+                b.ToTable("projects");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Name).IsRequired();
+                b.Property(x => x.Description);
+                b.Property(x => x.CreatedAt).HasDefaultValueSql("now()");
+            });
+        }
     }
 }
