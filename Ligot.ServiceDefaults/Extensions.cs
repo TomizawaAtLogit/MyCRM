1cf9c217c44794915cf63e19acc884a3219b6b9a
Eiji Tomizawa <tomizawa@logit.co.jp>
2026-03-27 14:35:57 +0900

    Enhance resilience handler configuration with custom timeout settings

--- Ligot.ServiceDefaults/Extensions.cs
+++ Ligot.ServiceDefaults/Extensions.cs
@@ -26,7 +26,12 @@ public static class Extensions
         builder.Services.ConfigureHttpClientDefaults(http =>
         {
             // Turn on resilience by default
-            http.AddStandardResilienceHandler();
+            http.AddStandardResilienceHandler(options =>
+            {
+                options.AttemptTimeout.Timeout = TimeSpan.FromSeconds(30);
+                options.TotalRequestTimeout.Timeout = TimeSpan.FromMinutes(2);
+                options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(60);
+            });
 
             // Turn on service discovery by default
             http.AddServiceDiscovery();
